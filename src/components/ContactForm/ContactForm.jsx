ccf105f5b1f0ac847d341637ebc36fcba9e32234
Maryna <marinakukusua@gmail.com>
2026-03-08 16:44:16 +0200

    Add yup and formik

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,96 +1,128 @@
 import { useDispatch, useSelector } from 'react-redux';
-import { useState, useEffect } from 'react';
 import { nanoid } from 'nanoid';
-import ContactFormInput from '../ContactFormInput/ContactFormInput';
+import * as yup from 'yup';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import { Button, TextField, Box } from '@mui/material';
 import {
   deleteContactItemAsync,
   updateContactItemAsync,
   addContactItemAsync,
+  selectContact,
 } from './../../store/slices/contactSlices';
+import { createNewContact } from '../../model/initialState';
 import styles from './ContactForm.module.css';
+
+const schema = yup.object().shape({
+  email: yup
+    .string()
+    .email('Невірний формат email')
+    .required('Введіть Email'),
+  phone: yup
+    .string()
+    .min(10, 'Номер телефону має містити мінімум 10 символів')
+    .required('Введіть номер телефону'),
+});
 
 function ContactForm () {
   const dispatch = useDispatch();
   const contactItem = useSelector(state => state.contactSlice.contactItem);
-  const [contact, setContact] = useState(contactItem);
 
-  useEffect(() => {
-    setContact(contactItem);
-  }, [contactItem]);
-
-  const handleChange = ev => {
-    const { name, value } = ev.target;
-    setContact(prev => ({
-      ...prev,
-      [name]: value,
-    }));
-  };
-
-  const clearSelectInput = fieldName => {
-    setContact(prev => ({
-      ...prev,
-      [fieldName]: '',
-    }));
-  };
-
-  const onSaveContact = ev => {
-    ev.preventDefault();
-    if (!contact.id) {
-      const newContact = { ...contact, id: nanoid() };
+  const onSaveContact = (values, { resetForm }) => {
+    console.log(values);
+    if (!values.id) {
+      const newContact = { ...values, id: nanoid() };
       dispatch(addContactItemAsync(newContact));
+      resetForm();
     } else {
-      dispatch(updateContactItemAsync(contact));
+      dispatch(updateContactItemAsync(values));
     }
   };
 
-  const handleDelete = ev => {
-    ev.preventDefault();
-    console.log(contact.id)
-    dispatch(deleteContactItemAsync(contact.id));
+  const handleDelete = () => {
+    dispatch(deleteContactItemAsync(contactItem.id));
+  };
+
+  const renderForm = ({ values, setFieldValue, isValid, dirty }) => {
+    return (
+      <Form className={styles['redaction-contact-div']}>
+        <Box position='relative'>
+          <Field
+            as={TextField}
+            name='firstName'
+            placeholder='First Name'
+            size='small'
+          />
+          {values.firstName && (
+            <span
+              className={styles.clearX}
+              onClick={() => setFieldValue('firstName', '')}
+            >
+              ✕
+            </span>
+          )}
+        </Box>
+        <Box position='relative'>
+          <Field name='lastName' placeholder='Last Name' />
+          {values.lastName && (
+            <span
+              className={styles.clearX}
+              onClick={() => setFieldValue('lastName', '')}
+            >
+              ✕
+            </span>
+          )}
+        </Box>
+        <Box position='relative'>
+          <Field name='email' placeholder='Email' />
+          {values.email && (
+            <span
+              className={styles.clearX}
+              onClick={() => setFieldValue('email', '')}
+            >
+              ✕
+            </span>
+          )}
+          <ErrorMessage name='email' component='div' />
+        </Box>
+        <Box position='relative'>
+          <Field name='phone' placeholder='Phone' />
+          {values.phone && (
+            <span
+              className={styles.clearX}
+              onClick={() => setFieldValue('phone', '')}
+            >
+              ✕
+            </span>
+          )}
+          <ErrorMessage name='phone' component='div' className={styles.error} />
+        </Box>
+        <div className={styles['divSaveAndDelete']}>
+          <Button
+            type='submit'
+            variant='contained'
+            disabled={!dirty || !isValid}
+          >
+            Save
+          </Button>
+          {contactItem.id !== null && (
+            <Button type='button' variant='outlined' onClick={handleDelete}>
+              Delete
+            </Button>
+          )}
+        </div>
+      </Form>
+    );
   };
 
   return (
-    <>
-      <form className={styles['redaction-contact-div']}>
-        <ContactFormInput
-          name='firstName'
-          placeholder='First Name'
-          value={contact.firstName}
-          handleChange={handleChange}
-          clearInput={clearSelectInput}
-        />
-
-        <ContactFormInput
-          name='lastName'
-          placeholder='Last Name'
-          value={contact.lastName}
-          handleChange={handleChange}
-          clearInput={clearSelectInput}
-        />
-
-        <ContactFormInput
-          name='email'
-          placeholder='Email'
-          value={contact.email}
-          handleChange={handleChange}
-          clearInput={clearSelectInput}
-        />
-
-        <ContactFormInput
-          name='phone'
-          placeholder='Phone'
-          value={contact.phone}
-          handleChange={handleChange}
-          clearInput={clearSelectInput}
-        />
-        <div className={styles['divSaveAndDelete']}>
-          <button onClick={onSaveContact}>Save</button>
-          {contact.id !== null && (
-            <button onClick={handleDelete}>Delete</button>
-          )}
-        </div>
-      </form>
-    </>
+    <Formik
+      enableReinitialize
+      initialValues={contactItem}
+      validationSchema={schema}
+      onSubmit={onSaveContact}
+    >
+      {renderForm}
+    </Formik>
   );
 }
 
--- src/components/ContactFormInput/ContactFormInput.jsx
+++ src/components/ContactFormInput/ContactFormInput.jsx
@@ -2,26 +2,26 @@ import styles from './ContactFormInput.module.css';
 
 
 function ContactFormInput ({ name, placeholder, value , handleChange, clearInput}) {
- const onClearInput = ev => {
-    ev.stopPropagation()
-    clearInput(name)
-  }
+//  const onClearInput = ev => {
+//     ev.stopPropagation()
+//     clearInput(name)
+//   }
 
-  return (
-    <div className={styles.inputWrapper}>
-      <input
-        name={name}
-        placeholder={placeholder}
-        value={value}
-        onChange={handleChange}
-      />
-      {value && (
-        <span className={styles.clearX} onClick={onClearInput}>
-          ✕
-        </span>
-      )}
-    </div>
-  );
+//   return (
+//     <div className={styles.inputWrapper}>
+//       <input
+//         name={name}
+//         placeholder={placeholder}
+//         value={value}
+//         onChange={handleChange}
+//       />
+//       {value && (
+//         <span className={styles.clearX} onClick={onClearInput}>
+//           ✕
+//         </span>
+//       )}
+//     </div>
+//   );
 }
 
 export default ContactFormInput;
